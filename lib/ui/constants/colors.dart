5e460b84a38421a147fab7ad6df83738f140f3f1
kamtou boris <boriskamtou@gmail.com>
2020-10-26 01:30:25 +0200

    setup theme & colors

---
+++ lib/ui/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const buttonColor = const Color(0xFFFA4A0C);
+  static const primaryColor = const Color(0xFFFA4A0C);
+  static const scaffoldBackgroundColor = const Color(0xFFFBCBABA);
+  static const cardWhiteBackgroundColor = const Color(0xFFFFFFFF);
+  static Color greyTextColor = Color(0xFF000000).withOpacity(.5);
+  static const colorBlack = const Color(0xFF000000);
+  static const iconGreyColor = const Color(0xFFC7C7C7);
+  static const unselectedElementColor = const Color(0xFF9A9A9D);
+}
---
+++ lib/ui/themes/themes.dart
@@ -0,0 +1,8 @@
+import 'package:boujee/ui/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+final myTheme = ThemeData(
+  buttonColor: AppColors.buttonColor,
+  scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor,
+  unselectedWidgetColor:  AppColors.unselectedElementColor,
+);
